37a3b2fdc9d39c8094b55fc087223747fe2b22f5
Rafael Matuoka <rmatuoka@korewa.com.br>
2011-03-22 18:20:37 -0300

    teste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TesteGit::Application.config.secret_token = 'e2350fded15a3b7352a232746ad06d67805430bc86e58551070c0a73d96a36a3de60e0b62f362999311f212eb0aa8812303790ba03797c8c3088de9b77310625'
